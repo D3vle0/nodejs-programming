8f08dd1724e837559235d640273a8ca104bcd072
D3vle0 <choice7203@dimigo.hs.kr>
2022-04-02 01:54:39 +0900

    0401

---
+++ basic-web/syntax/conditional.js
@@ -0,0 +1,15 @@
+const args = process.argv;
+
+console.log(args[2], args[3])
+
+console.log("A");
+console.log("B");
+
+if (args[2] == "emiya"){
+    console.log("C1")
+}
+else {
+    console.log("C2")
+}
+
+console.log("D");
