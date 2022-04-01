658cfc6800e5568fa91bd6f189e153e016b292c8
D3vle0 <choice7203@dimigo.hs.kr>
2022-04-01 15:30:39 +0900

    0401

--- basic-web/index.js
+++ basic-web/index.js
@@ -6,6 +6,7 @@ const url = require("url");
 const app = http.createServer((req, res) => {
     var _url = req.url;
     const queryData = url.parse(_url, 1).query;
+    var title = queryData.id;
     console.log(queryData.name);
     if (req.url == "/") {
         _url = "/index.html";
@@ -14,9 +15,31 @@ const app = http.createServer((req, res) => {
         return res.writeHead(404);
     }
     res.writeHead(200);
-    res.end(fs.readFileSync(__dirname + _url));
+    fs.readFile(`data/${queryData.id}`, "utf-8", function (err, description) {
+        var template = `<!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - HTML</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="index.html">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=Javascript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+
+        res.end(template);
+    })
+    // res.end(fs.readFileSync(__dirname + _url));
     // res.end(queryData.name);
-    console.log(queryData.name);
+    // console.log(queryData.name);
 });
 
 app.listen(65535);
